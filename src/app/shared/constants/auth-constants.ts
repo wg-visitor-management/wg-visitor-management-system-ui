8499f75cffff0ca97dbfe7993f67c288541c0d0f
abkumar-wg <Abhishek.kumar@watchguard.com>
2024-03-06 17:05:16 +0530

    cleaning up

--- src/app/shared/constants/auth-constants.ts
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONSTANTS = {
-  USERNAME: 'Username',
-  PASSWORD: 'Password',
-  BUTTON_TEXT: 'Login',
-  FORM_HEADING: 'Welcome',
-}
--- src/app/shared/constants/auth-service-constants.ts
+++
@@ -1,3 +0,0 @@
-export const AUTH_SERVICE_CONSTANTS = {
-    baseUrl: "https://0s144isb54.execute-api.ap-south-1.amazonaws.com/dev"
-}
